71a79c76dc4bc191065afe4eaaf89c64015b48eb
Alyf  Mogades <alyfnaruto@gmail.com>
2022-08-05 11:17:50 +0700

    first commit

---
+++ resources/views/includes/promote/head.blade.php
@@ -0,0 +1,23 @@
+{{-- {{ asset('template/promote/ ')}} --}}
+<link rel="apple-touch-icon')}}" href="{{ asset('temp;ate/promote/assets/img/apple-icon.png') }}">
+<link rel="shortcut icon')}}" type="image/x-icon" href="{{ asset('template/promote/assets/img/favicon.ico')}}">
+
+<link rel="stylesheet" href="{{ asset('template/promote/assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{ asset('template/promote/assets/css/templatemo.css')}}">
+<link rel="stylesheet" href="{{ asset('template/promote/assets/css/custom.css')}}">
+
+<!-- Load fonts style after rendering the layout styles -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+<link rel="stylesheet" href="{{ asset('template/promote/assets/css/fontawesome.min.css')}}">
+<!--
+
+TemplateMo 559 Zay Shop
+
+https://templatemo.com/tm-559-zay-shop
+
+-->
+<script src="{{ asset('template/promote/assets/js/jquery-1.11.0.min.js')}}"></script>
+<script src="{{ asset('template/promote/assets/js/jquery-migrate-1.2.1.min.js')}}"></script>
+<script src="{{ asset('template/promote/assets/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('template/promote/assets/js/templatemo.js')}}"></script>
+<script src="{{ asset('template/promote/assets/js/custom.js')}}"></script>
