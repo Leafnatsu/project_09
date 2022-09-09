23b7d67aa4be81642568bc187a363392f7afdaca
Leafnatsu <alyfnaruto@gmail.com>
2022-09-09 09:13:45 +0700

    ..

--- resources/views/adminpage/about/adminabout.blade.php
+++ resources/views/adminpage/about/adminabout.blade.php
@@ -4,7 +4,7 @@
     <!-- Content -->
 
     <div class="container-xxl flex-grow-1 container-p-y">
-      <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables</h4>
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span>About</h4>
    <a href="/admin/about/from" class="btn btn-primary"> 
      
     ปุ่มเพิ่ม
