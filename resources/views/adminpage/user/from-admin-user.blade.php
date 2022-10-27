3fde201485c282c157c74786b8f412d77666ed37
Leafnatsu <alyfnaruto@gmail.com>
2022-10-27 09:11:59 +0700

    from-edit user

--- resources/views/adminpage/user/from-admin-user.blade.php
+++ resources/views/adminpage/user/from-admin-user.blade.php
@@ -13,21 +13,31 @@
                 <div class="row">
                 <div class="col-md-11.5">
                   <div class="card mb-6">
-                    <h5 class="card-header">Basic</h5>
+                    <h5 class="card-header">Name</h5>
                     <div class="card-body demo-vertical-spacing demo-only-element">
                       <div class="input-group">
-                        <span class="input-group-text" id="basic-addon11">@</span>
                         <input
                           type="text"
                           class="form-control"
-                          placeholder="Username"
-                          aria-label="Username"
+                          placeholder="Name"
+                          aria-label="Name"
                           aria-describedby="basic-addon11"
                         />
                       </div>
 
+                      <h5>Email</h5>
+                        <div class="input-group">
+                          <input
+                            type="text"
+                            class="form-control"
+                            placeholder="Email"
+                            aria-label="Email"
+                            aria-describedby="basic-addon11"
+                          />
+                        </div>
+
                       <div class="form-password-toggle">
-                        <label class="form-label" for="basic-default-password12">Password</label>
+                      <h5>Password</h5>
                         <div class="input-group">
                           <input
                             type="password"
@@ -42,127 +52,19 @@
                         </div>
                       </div>
 
+                      <h5>Phone</h5>
                       <div class="input-group">
                         <input
                           type="text"
                           class="form-control"
-                          placeholder="Recipient's username"
-                          aria-label="Recipient's username"
-                          aria-describedby="basic-addon13"
+                          placeholder="Phone"
+                          aria-label="Phone"
+                          aria-describedby="basic-addon11"
                         />
-                        <span class="input-group-text" id="basic-addon13">@example.com</span>
-                      </div>
-
-                      <div class="input-group">
-                        <span class="input-group-text" id="basic-addon14">https://example.com/users/</span>
-                        <input
-                          type="text"
-                          class="form-control"
-                          placeholder="URL"
-                          id="basic-url1"
-                          aria-describedby="basic-addon14"
-                        />
-                      </div>
-
-                      <div class="input-group">
-                        <span class="input-group-text">$</span>
-                        <input
-                          type="text"
-                          class="form-control"
-                          placeholder="Amount"
-                          aria-label="Amount (to the nearest dollar)"
-                        />
-                        <span class="input-group-text">.00</span>
-                      </div>
-
-                      <div class="input-group">
-                        <span class="input-group-text">With textarea</span>
-                        <textarea class="form-control" aria-label="With textarea" placeholder="Comment"></textarea>
                       </div>
                     </div>
                   </div>
                 </div>
-              <!-- Custom file input -->
-              <div class="row">
-                <div class="col-12">
-                  <div class="card">
-                    <h5 class="card-header">Custom file input</h5>
-                    <div class="card-body demo-vertical-spacing demo-only-element">
-                      <div class="input-group">
-                        <label class="input-group-text" for="inputGroupFile01">Upload</label>
-                        <input type="file" class="form-control" id="inputGroupFile01" />
-                      </div>
-
-                      <div class="input-group">
-                        <input type="file" class="form-control" id="inputGroupFile02" />
-                        <label class="input-group-text" for="inputGroupFile02">Upload</label>
-                      </div>
-
-                      <div class="input-group">
-                        <button class="btn btn-outline-primary" type="button" id="inputGroupFileAddon03">Button</button>
-                        <input
-                          type="file"
-                          class="form-control"
-                          id="inputGroupFile03"
-                          aria-describedby="inputGroupFileAddon03"
-                          aria-label="Upload"
-                        />
-                      </div>
-
-                      <div class="input-group">
-                        <input
-                          type="file"
-                          class="form-control"
-                          id="inputGroupFile04"
-                          aria-describedby="inputGroupFileAddon04"
-                          aria-label="Upload"
-                        />
-                        <button class="btn btn-outline-primary" type="button" id="inputGroupFileAddon04">Button</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
             <!-- / Content -->
-
-            <!-- Footer -->
-            <footer class="content-footer footer bg-footer-theme">
-              <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                <div class="mb-2 mb-md-0">
-                  ©
-                  <script>
-                    document.write(new Date().getFullYear());
-                  </script>
-                  , made with ❤️ by
-                  <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
-                </div>
-                <div>
-                  <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                  <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
-                  <a
-                    href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
-                    target="_blank"
-                    class="footer-link me-4"
-                    >Documentation</a
-                  >
-
-                  <a
-                    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-                    target="_blank"
-                    class="footer-link me-4"
-                    >Support</a
-                  >
-                </div>
-              </div>
-            </footer>
-            <!-- / Footer -->
-
-            <div class="content-backdrop fade"></div>
-          </div>
-          <!-- Content wrapper -->
-        </div>
-        <!-- / Layout page -->
-      </div>
       @stop
